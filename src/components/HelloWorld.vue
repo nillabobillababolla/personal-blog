ab242499f5067bf433e74a8626e0419e30a45640
Samet Alabey <samet.alabey@hotmail.com>
2023-04-16 04:16:36 +0300

    update: HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,17 +9,6 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
